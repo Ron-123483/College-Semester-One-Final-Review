54c0b339fafc051227a6a1a24f62fa40e5054b0f
RoNaK <164240613+Ron-123483@users.noreply.github.com>
2024-03-29 12:43:23 -0700

    Add files via upload

---
+++ Finalreview.java
@@ -0,0 +1,366 @@
+//import java.util.Scanner;
+//
+//public class Finalreview {
+//    public static void main(String[] args) {
+//
+//        Scanner in = new Scanner(System.in);
+
+       // long ccnumber;
+
+
+
+//        for(int i = 0; i < 7; i++) {
+//
+//            for (int j = 0; j < 7; j++){
+//
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        }
+//    }
+
+
+
+
+
+
+//        while(true){
+//            System.out.println("enter a cc number");
+//            if(in.hasNextLong()){
+//                ccnumber = in.nextLong();
+//                break;
+//
+//
+//            }
+//
+//
+//
+//        }
+
+
+
+
+
+//    }
+//
+//
+//
+//
+//
+//
+//
+//}
+
+
+
+//import java.util.Scanner;
+//
+//        public class Finalreview {
+//            public static void main(String[] args) {
+//
+//                Scanner in = new Scanner(System.in);
+
+
+//                for(int i = 0; i < 11; i++){
+//                    System.out.println(i);
+//
+//
+//                }
+
+
+//                    int sum = 0;
+//
+//                    for (int i = 1; i <= 10; i++) {
+//                          sum += i;
+//                    }
+//                        System.out.println("Sum: " + sum);
+//
+//                    }
+//                    }
+
+
+//                int sum = 0;
+//                for (int i = 1; i <= 10; i++) {
+//                    sum += i;
+//                }
+//                System.out.println("Sum: " + sum);
+//            }}
+
+
+//                int x = 0;
+//                while(x < 4) {
+//                    x = x + 1;
+//                }
+//                    System.out.println("x is : " + x);
+
+//                int count = 0;
+//                do{
+//
+//                    System.out.println("Welcome to java ");
+//                    count++;
+//
+//
+//                }while(count < 10);
+
+//                    number -= 3;
+//                    System.out.print(number + " ");
+//                }
+
+//                int count = 0;
+//                while(count < 10){
+//
+//                    System.out.println("welcome to java");
+//                    System.out.println(count);
+//                    count++;
+//                }
+
+
+//                double sum = 0;
+//                for(double d = 0; d < 10;){
+//
+//                    d += 0.1;
+//                    sum += d;
+//
+//                    System.out.println(sum);
+
+//                for (int count = 0; count < 10; count++){
+//
+//                    System.out.println("Welcome to java");
+//
+//                }
+
+//                for(int i = 1; i <= 10; ++i){
+//                    System.out.print(i + "");
+//                    ++i;
+//
+//                }
+
+//            int i = 5;
+//            int result = i++;
+//            System.out.println(i);
+//            System.out.println(result);
+
+
+//                int y = 0;
+//                for(int i = 0; i < 10; ++i){
+//
+//                    y += i;
+//                    System.out.println(y);
+//
+//                }
+
+
+//            for (int i = 0;i < 10; i++){
+//                for(int j = 0; j < 10; j++){
+//                    System.out.println(i * j);
+//                }
+//            }
+
+//                for (int i = 0; i < 10; i++){
+//                    for(int j = 0; j < i; j++){
+//
+//                        System.out.println(i * j);
+//
+//                    }
+//
+//
+//                }
+
+
+
+
+
+
+//import java.util.Scanner;
+//
+//public class Finalreview {
+//    public static void main(String[] args) {
+//
+//        Scanner in = new Scanner(System.in);
+//        int balance = 10;
+//        while (true) {
+//            if (balance < 9)
+//                break;
+//            balance = balance - 9;
+
+
+//        int sum = 3;
+//        int item = 2;
+//
+//        do{
+//            item++;
+//            sum += item;
+//            if (sum > 4);
+//            break;
+//
+//
+//        }while(item < 5);
+//
+//        System.out.println(sum);
+//        System.out.println(item);
+//
+//            }
+//        }
+
+//        int i = 2;
+//        int j = 2;
+//
+//        int k = j += i;
+//
+//        System.out.println(k);
+//
+//    }
+//}
+//
+
+
+//
+
+//
+//        int sum = 0;
+//        int item = 0;
+//
+//        do {
+//
+//            item++;
+//            if (sum >= 4) {
+//                continue;
+//            }
+//            sum += item;
+//        } while (item < 5);
+//
+//        System.out.print(sum);
+//    }
+//
+//}
+
+//        int balance = 10;
+//
+//        while (balance >= 1) {
+//            if (balance < 9) {
+//                continue;
+//
+//            }
+//            balance = balance - 9;
+//
+//            System.out.println(balance);
+//
+//        }
+//    }
+//}
+
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+//import java.util.Scanner;
+//
+//public class Finalreview {
+//    public static void main(String[] args) {
+//
+//        Scanner in = new Scanner(System.in);
+
+//        double [] myList = {1, 5, 5, 5, 5, 1};
+//
+//        double max = myList[0];
+//        int indexOfMax = 0;
+//        for(int i = 1; i < myList.length; i++){
+//            if (myList[i] > max){
+//
+//                max = myList[i];
+//                indexOfMax = i;
+//            }
+//        }
+//        System.out.println(indexOfMax);
+
+
+
+//        int [] x = new int[5];
+//        int i;
+//        for(i = 0; i < x.length; i++){
+//            x[i] = i;
+//
+//            System.out.println(x[i]);
+//
+//        }
+
+
+
+//        final int [] x = {1,2,3,4};
+//        int [] y = x;
+//
+//        x = new int[2];
+//
+//        for (int i = 0; i < y.length; i++){
+//
+//            System.out.println(y[i] + "");
+//
+//        }
+
+//        int[] list = new int[5];
+//        list = new int[6];
+
+
+
+//        int [] x = {1,2,3,4};
+//        int[] y = x;
+//
+//        x = new int[2];
+//
+//        for(int i = 0; i < x.length; i++){
+//            System.out.println(x[i] + "");
+//        }
+
+
+
+//        int[][][] x = {{{1, 2}, {3,4}}, {{5,6,}, {7,8}}};
+//
+//        System.out.println(x[1][0][0]);
+
+//    }
+//}
+
+
+
+import java.util.*;
+
+
+import java.util.Scanner;
+
+//public class Finalreview {
+//    public static void printMyName(String name) {
+//        System.out.println(name);
+//        return;
+//    }
+//
+//
+//        public static void main(String[] args[]) {
+//
+//            Scanner sc = new Scanner(System.in);
+//            String name = sc.next();
+//
+//            printMyName(name);  // called function
+//
+//            sc.close(); // close the Scanner
+//
+//
+//
+//        }
+//    }
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
